0830c4cd55aeadb47da0e1723ee297e7f0286c0a
Eduardo de Moraes <moraes.eduardo@grad.ufsc.br>
2025-09-25 21:34:06 -0300

    Inicio do desafio

---
+++ src/main/java/com/bry/desafio/DesafioBryApplication.java
@@ -0,0 +1,13 @@
+package com.bry.desafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioBryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioBryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bry/desafio/DesafioBryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bry.desafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioBryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
